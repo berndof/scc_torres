2bb17641b29033bc157c2c6fef84367947490c35
berndof <bernardofb@gmail.com>
2025-11-17 20:14:16 -0300

    home frontend

--- frontend/src/routes/login/+page.server.ts
+++ frontend/src/routes/login/+page.server.ts
@@ -1,14 +1,26 @@
 import type { Actions } from './$types';
+import { redirect, fail} from '@sveltejs/kit';
+import { do_login_request } from "$lib/server/auth"
 
 export const actions = {
-    login: async ({ request }) => {
-        const formData = await request.formData();
+    login: async (event) => {
+    const res = await do_login_request(event)
+    const data = await res.json();
+    //tratar resposta
+    if (!res.ok) {
+      return fail(res.status, { error: data.detail });
+    };
 
-        const username = formData.get('username')?.toString();
-        const password = formData.get('password')?.toString();
-        const scope = formData.get('scope')?.toString();
+    event.cookies.set("access_token", data.access_token, {
+      path: '/',
+      httpOnly: true, 
+      sameSite: 'strict', 
+      secure: false
+    });
 
-        console.log(formData)
+    //salvei o bearer token nos cookies 
+
+    throw redirect(302, '/home')
 
     }
 } satisfies Actions
