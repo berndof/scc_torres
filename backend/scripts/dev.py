839990fa4c9a65cc148040b36822dc9fb90710bb
berndof <bernardofb@gmail.com>
2025-11-17 18:55:13 -0300

    env backend - docker

---
+++ backend/scripts/dev.py
@@ -0,0 +1,22 @@
+import sys
+from pathlib import Path
+
+import uvicorn
+
+sys.path.insert(0, str(Path(__file__).parent.parent / "src"))
+
+from core.config import APP_LISTEN_ADDR, APP_LISTEN_PORT
+from core.log import LOG_CONFIG
+
+if __name__ == "__main__":
+    uvicorn.run(
+        app="main:app",
+        host=APP_LISTEN_ADDR,
+        port=APP_LISTEN_PORT,
+        log_config=LOG_CONFIG,
+        # access_log=True,
+        reload=True,
+        loop="uvloop",
+        reload_dirs=["src/"],
+        reload_delay=1.0,
+    )
